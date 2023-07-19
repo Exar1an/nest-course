8d943bcc5e7a2b60e7115d194fbe92978b3d9f81
Exar1an <79934217+Exar1an@users.noreply.github.com>
2023-07-19 18:26:01 +0300

    Add files via upload

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { User } from "../users/users.model";
+import { UserRoles } from "./user-roles.model";
+
+
+interface RoleCreationAttrs {
+    value: string;
+    description: string;
+}
+
+
+@Table({tableName: 'roles'})
+export class Role extends Model<Role> {
+
+    @ApiProperty({example: '1', description: 'Unique id.'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @ApiProperty({example: 'Admin', description: 'Role name.'})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    value: string;
+
+    @ApiProperty({example: 'АДМИНИСТРАТОР', description: 'Описание роли.'})
+    @Column({type: DataType.STRING, allowNull: false})
+    description: string;
+
+    @BelongsToMany(() => User, () => UserRoles)
+    users: User[] 
+
+}
